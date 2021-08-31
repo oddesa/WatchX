610c4ca4330696345ef6ee317d7d103fb4d6c972
oddesa <jehnsen.hirena@sci.ui.ac.id>
2021-08-31 21:22:05 +0700

    Initial Commit

---
+++ WatchX/WatchXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchXApp.swift
+//  WatchX
+//
+//  Created by Jehnsen Kane on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
